49886b9e1b33c99717b2b55ba8107343b9f2f634
Masood Moosavi <m4sood.m@gmail.com>
2025-04-13 11:52:38 +0330

    .

--- components/ui/Photo.js
+++ components/ui/Photo.js
@@ -21,7 +21,7 @@ const Photo = () => {
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
-            src="/assets/photo4.jpg"
+            src="/assets/Photo4.jpg"
             priority
             quality={100}
             alt="Profile picture"
